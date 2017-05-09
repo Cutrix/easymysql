34e68bb9bcaafbf1ae298dbb61b9c39b719f8316
Cutrix <houessinonlandry@gmail.com>
2017-05-09 02:19:54 +0000

    defintion d'une constante "eerything_selector"

--- easymysql.php
+++ easymysql.php
@@ -12,6 +12,7 @@ declare (strict_types=1);
 class easymysql {
     
     private $db;
+    const EVERYTHING_SELECTOR = "SELECT * FROM ";    
 
 
     public function __construct($db) {
@@ -28,7 +29,7 @@ class easymysql {
     
     public function getFromMysql(string $table, int $fetch = 0, string ...$arg) {
         if (empty($arg)) {
-            $q = $this->db->prepare("SELECT * FROM $table");
+            $q = $this->db->prepare(self::EVERYTHING_SELECTOR.$table);
             $q->execute();
             return $q->fetchAll($fetch);
         }
@@ -69,7 +70,7 @@ class easymysql {
     
     public function getFromMysqlOptions(string $table, int $fetch, array $options, array $values, string ...$arg) {
         if (empty($arg)) {            
-            $q = $this->db->prepare('SELECT * FROM '.$table.' WHERE '.$this->walk($options, "=?, ").'=?');
+            $q = $this->db->prepare(self::EVERYTHING_SELECTOR.$table.' WHERE '.$this->walk($options, "=?, ").'=?');
             $q->execute(array($this->walk($values)));
             return $q->fetchAll($fetch);
         }
@@ -138,7 +139,7 @@ class easymysql {
     public function searchFromMysql(string $table, $search, int $fetch = 0, string $column_id = 'id', string ...$arg) {
         if (empty($arg)) {
             if (gettype($search) == 'integer') {
-                $q = $this->db->prepare("SELECT * FROM ".$table." WHERE ".$column_id." = ".$search);
+                $q = $this->db->prepare(self::EVERYTHING_SELECTOR.$table." WHERE ".$column_id." = ".$search);
                 $q->execute();
                 return $q->fetchAll($fetch);
             }
